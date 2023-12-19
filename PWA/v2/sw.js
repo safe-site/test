510d0f93ea5eb99df15e1f7ee4c21d3735b0b713
Shashi Kumar <contact9508914855@gmail.com>
2023-12-19 20:21:03 +0530

    b

--- PWA/v2/sw.js
+++ PWA/v2/sw.js
@@ -1,22 +1,21 @@
 // Service Worker Installation
 self.addEventListener("install", (e) => {
-    e.waitUntil(
+  e.waitUntil(
       caches.open("PWA-static").then((cache) => {
-        return cache.addAll([
-          "./",
-          "./src/index.css",
-          "/images/logo192.png"
-        ]);
+          return cache.addAll([
+              "./",
+              "./src/index.css",
+              "./images/logo192.png"  // Corrected path
+          ]);
       })
-    );
-  });
-  
-  // Service Worker Fetch
-  self.addEventListener("fetch", (e) => {
-    e.respondWith(
+  );
+});
+
+// Service Worker Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
       caches.match(e.request).then((response) => {
-        return response || fetch(e.request);
+          return response || fetch(e.request);
       })
-    );
-  });
-  
+  );
+});
