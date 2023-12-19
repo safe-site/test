541871ec4a6b0873d816c92757335fc5df5c1362
Shashi Kumar <contact9508914855@gmail.com>
2023-12-19 19:47:34 +0530

    v

--- PWA/v2 copy/sw.js
+++ PWA/v2 copy/sw.js
@@ -1,10 +1,10 @@
 // Service Worker Installation
 self.addEventListener("install", (e) => {
     e.waitUntil(
-      caches.open("PWA-static").then((cache) => {
+      caches.open("static").then((cache) => {
         return cache.addAll([
           "./",
-          "./src/index.css",
+          "./src/master.css",
           "/images/logo192.png"
         ]);
       })
